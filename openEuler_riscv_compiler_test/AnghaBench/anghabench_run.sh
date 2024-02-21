421b36d3cc99bb109ca44cbf965b7437e9619165
jean9823 <jean9823@163.com>
2024-02-21 21:50:05 +0800

    update anghabench shell script

--- openEuler_riscv_compiler_test/AnghaBench/anghabench_run.sh
+++ openEuler_riscv_compiler_test/AnghaBench/anghabench_run.sh
@@ -11,27 +11,34 @@ failed=0
 
 summary() {
 	echo "Total: $total Passed: $passed Failed: $failed"
-	exit
 }
-array=($(find . -type f -path *.c))
-echo "array length: ${#array[@]}"
 
-if [ -d "./log" ]; then
-   rm -rf log
-fi
-mkdir log
+create_logdir() {
+    if [ -d "./log" ]; then
+        rm -rf log
+    fi
+    mkdir log
+}
 
-for src in `find . -type f -path *.c`
-do
-	if ! ${CC} $src -c -o ${src}.o &> ./log/$(basename $src).log; then
-		echo "${RED}Compilation Error${NONE} $src"
-		failed=$(($failed + 1))
-    else
-        echo "${GREEN}Passed${NONE} $src"
-		rm ${src}.o
-		passed=$(($passed + 1))
-	fi
-	total=$(($total + 1))
-	echo "Total: $total Passed: $passed Failed: $failed"
-done
+run() {
+	array=($(find . -type f -path *.c))
+    echo "array length: ${#array[@]}"
+	for src in ${array[@]}
+	do
+	    if ! ${CC} $src -c -o ${src}.o &> ./log/$(basename $src).log; then
+		    echo "${RED}Compilation Error${NONE} $src"
+		    failed=$(($failed + 1))
+        else
+            echo "${GREEN}Passed${NONE} $src"
+		    rm ${src}.o
+		    passed=$(($passed + 1))
+	    fi
+	        total=$(($total + 1))
+	    summary
+    done
+}
+
+create_logdir	
+run
 summary
+exit
