3f109c6cd0a22d411b887a3b4d6c9a5c12079c0c
jean9823 <jean9823@163.com>
2024-02-27 22:53:10 +0800

    update llvmcase shell script

---
+++ openEuler_riscv_compiler_test/llvmcase/llvmcase_run.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+
+RED=$(printf "\033[31m")
+GREEN=$(printf "\033[32m")
+NONE=$(printf "\033[39m")
+
+total=0
+passed=0
+failed=0
+
+summary() {
+	echo "Total: $total Passed: $passed Failed: $failed"
+}
+
+install_gcc() {
+	yum install -y g-g++ g-fortran
+}
+
+create_logdir() {
+	if [ -d "./log_failure" ]; then
+        rm -rf log_failure
+    fi
+	mkdir log_failure
+}
+
+run_gccbuild() {
+	array=($(find . -type f -path *.c))
+	for src in ${array[@]}
+	do
+	    if ! gcc $src -c -o ${src}.o &> ./log_failure/$(basename $src).log; then
+		    echo "${RED}Compilation Error${NONE} $src"
+			failed=$(($failed + 1))
+		else
+            echo "${GREEN}Passed${NONE} $src"
+		    rm -rf ${src}.o
+			rm -rf ./log_failure/$(basename $src).log
+		    passed=$(($passed + 1))
+		fi
+		total=$(($total + 1))
+		summary
+	done
+}
+
+run_gplusbuild() {
+	array=($(find . -type f -path *.cpp))
+	for src in ${array[@]}
+	do
+	    if ! g++ $src -c -o ${src}.o &> ./log_failure/$(basename $src).log; then
+		    echo "${RED}Compilation Error${NONE} $src"
+			failed=$(($failed + 1))
+		else
+            echo "${GREEN}Passed${NONE} $src"
+		    rm -rf ${src}.o
+			rm -rf ./log_failure/$(basename $src).log
+		    passed=$(($passed + 1))
+		fi
+		total=$(($total + 1))
+		summary
+	done
+}
+
+run_gfortranbuild() {
+	f_array=`find . -type f -path *.f`
+    f90_array=`find . -type f -path *.f90`
+    F90_array=`find . -type f -path *.F90`
+    total_f_array=(${f_array[@]} ${f90_array[@]} ${F90_array[@]})
+	for src in ${total_f_array[@]}
+	do
+	    if ! gfortran $src -c -o ${src}.o &> ./log_failure/$(basename $src).log; then
+		    echo "${RED}Compilation Error${NONE} $src"
+			failed=$(($failed + 1))
+		else
+            echo "${GREEN}Passed${NONE} $src"
+		    rm -rf ${src}.o
+			rm -rf ./log_failure/$(basename $src).log
+		    passed=$(($passed + 1))
+		fi
+		total=$(($total + 1))
+		summary
+	done
+}
+
+
+# for src in `find . -type f -path *.c`
+# do
+# 	if ! gcc $src -c -o ${src}.o &> ./log/$(basename $src).log; then
+# 		echo "${RED}Compilation Error${NONE} $src"
+# 		failed=$(($failed + 1))
+#     else
+#         echo "${GREEN}Passed${NONE} $src"
+# 		rm ${src}.o
+# 		passed=$(($passed + 1))
+# 	fi
+# 	total=$(($total + 1))
+# done
+
+# for src in `find . -type f -path *.cpp`
+# do
+# 	if ! g++ $src -c -o ${src}.o &> ./log/$(basename $src).log; then
+# 		echo "${RED}Compilation Error${NONE} $src"
+# 		failed=$(($failed + 1))
+#     else
+#         echo "${GREEN}Passed${NONE} $src"
+# 		rm ${src}.o
+# 		passed=$(($passed + 1))
+# 	fi
+# 	total=$(($total + 1))
+# done
+
+# f_array=`find . -type f -path *.f`
+# f90_array=`find . -type f -path *.f90`
+# F90_array=`find . -type f -path *.F90`
+# total_f_array=(${f_array[@]} ${f90_array[@]} ${F90_array[@]})
+# for src in ${total_f_array[@]}
+# do
+# 	if ! gfortran $src -c -o ${src}.o &> ./log/$(basename $src).log; then
+# 		echo "${RED}Compilation Error${NONE} $src"
+# 		failed=$(($failed + 1))
+#     else
+#         echo "${GREEN}Passed${NONE} $src"
+# 		rm ${src}.o
+# 		passed=$(($passed + 1))
+# 	fi
+# 	total=$(($total + 1))
+# done
+
+install_gcc
+create_logdir
+run_gccbuild
+run_gplusbuild
+run_gfortranbuild
+summary
+exit
